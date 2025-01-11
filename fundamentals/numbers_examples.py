5477ef672ebb8605dfcd01daa90cce0f6d4afd74
zpaulinz <zpaulinz@gmail.com>
2025-01-11 03:41:48 +0100

    Add numbers_examples.py

---
+++ fundamentals/numbers_examples.py
@@ -0,0 +1,28 @@
+#Integers
+print(2 + 3, 
+      3 - 2,
+      2 * 3,
+      3 / 2,
+      3 ** 2,
+      10 ** 8,
+      2 + 3*4,
+      (2 + 3) * 4)
+
+#Floating point numbers
+print(0.1 + 0.1,
+      0.2 + 0.2,
+      2 * 0.1,
+      2 * 0.2,
+       0.2 + 0.1,
+       3 * 0.1)
+
+#Integers and floating point numbers
+print(4/2,
+      1 + 2.0,
+      2 * 3.0,
+      3.0 ** 2)
+
+#Number underscores
+print(14_000_000_000)
+
+
