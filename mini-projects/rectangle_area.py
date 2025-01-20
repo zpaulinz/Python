db8c89036d76888e93a027ccca2b62d32fd430e8
zpaulinz <zpaulinz@gmail.com>
2025-01-20 23:09:10 +0100

    Add rectangle_area.py

---
+++ mini-projects/rectangle_area.py
@@ -0,0 +1,8 @@
+print(f"The program calculates the area of a rectangle.\n")
+
+a = float(input("Enter side a = "))
+b = float(input("Enter side b = ")) 
+
+area = a * b  
+
+print("The area of the rectangle =", area)  
