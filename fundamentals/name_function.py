96cc2c5d76e18f42ee06c7cb3aae53b2a5fc227e
zpaulinz <zpaulinz@gmail.com>
2025-01-29 00:02:18 +0100

    Make 'middle' argument optional in get_formatted_name

--- fundamentals/name_function.py
+++ fundamentals/name_function.py
@@ -1,5 +1,7 @@
-def get_formatted_name(first, middle, last):
+def get_formatted_name(first, last, middle=""):
     """Generates a neatly formatted full name."""
-    full_name = f"{first} {middle} {last}"
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
     return full_name.title()
-
