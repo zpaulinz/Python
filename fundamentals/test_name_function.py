efa991cce851df8755064cae38bf7a1d2aabf415
zpaulinz <zpaulinz@gmail.com>
2025-01-29 00:08:16 +0100

    Refactor functions by removing 'self' argument

--- fundamentals/test_name_function.py
+++ fundamentals/test_name_function.py
@@ -1,11 +1,11 @@
 from name_function import get_formatted_name
 
-def test_first_last_name(self):
+def test_first_last_name():
     """Are the data in the form 'Janis Joplin' handled correctly?"""
     formatted_name = get_formatted_name('janis', 'joplin')
     assert formatted_name == 'Janis Joplin'
 
-def test_first_last_middle_name(self):
+def test_first_last_middle_name():
     """Are the data in the form 'Wolfgang Amadeus Mozart' handled correctly?"""
     formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
     assert formatted_name == 'Wolfgang Amadeus Mozart'
