27d8c61a6a958a0b9052e3ef16048a51b5471c65
zpaulinz <zpaulinz@gmail.com>
2025-01-28 23:59:08 +0100

    Add 'middle' argument to get_formatted_name

--- fundamentals/name_function.py
+++ fundamentals/name_function.py
@@ -1,5 +1,5 @@
-def get_formatted_name(first, last):
+def get_formatted_name(first, middle, last):
     """Generates a neatly formatted full name."""
-    full_name = f"{first} {last}"
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
 
