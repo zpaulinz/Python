0c11101257ae0a0eb9f01d0005079674d56aadd8
zpaulinz <zpaulinz@gmail.com>
2025-01-30 22:38:44 +0100

    Create code_testing.py

---
+++ fundamentals/code_testing.py
@@ -0,0 +1,17 @@
+# python3 -m pip install --upgrade pip
+# python3 -m pip install --upgrade package_name
+
+# python3 -m pip install --user package_name
+# python3 -m pip install --user pytest
+
+# Most commonly used assertion commands in tests:
+'''
+Assertion:	                Description:
+------------------------------------------------------------------------
+assert a == b	            Assumes that two values are equal.
+assert a != b	            Assumes that two values are not equal.
+assert a	                Assumes that a evaluates to True.
+assert not a	            Assumes that a evaluates to False.
+assert element in list	    Assumes that the element is in the list.
+assert element not in list	Assumes that the element is not in the list.
+'''
