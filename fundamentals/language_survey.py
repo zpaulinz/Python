d3f470eab795e7e4a1394849fcf73c571cdb58f8
zpaulinz <zpaulinz@gmail.com>
2025-01-31 01:40:30 +0100

    Create language_survey.py

---
+++ fundamentals/language_survey.py
@@ -0,0 +1,19 @@
+from survey import AnonymousSurvey
+
+# Define the question and create the survey.
+question = "What is your native language?"
+language_survey = AnonymousSurvey(question)
+
+# Display the question and store the responses.
+language_survey.show_question()
+print("Type 'q' to quit the program.\n")
+
+while True:
+    response = input("Language: ")
+    if response == 'q':
+        break
+    language_survey.store_response(response)
+
+# Display the survey results.
+print("\nThank you to everyone who participated in the survey!")
+language_survey.show_results()
