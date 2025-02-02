5abc09cb95317f739c9f6755a89439e717d5ee08
zpaulinz <zpaulinz@gmail.com>
2025-02-02 23:40:33 +0100

    Refactor tests with pytest fixture

--- fundamentals/test_survey.py
+++ fundamentals/test_survey.py
@@ -1,19 +1,30 @@
+import pytest
 from survey import AnonymousSurvey
 
-def test_store_single_response(): 
-    """Check if a single response is stored correctly."""
+@pytest.fixture 
+def language_survey(): 
+    """
+    Create a survey for use in all test functions.
+    """
     question = "What is your native language?"
     language_survey = AnonymousSurvey(question)
+    return language_survey
+    
+
+def test_store_single_response(language_survey): 
+    """Check if a single response is stored correctly."""
     language_survey.store_response('Polish')
     assert 'Polish' in language_survey.responses
 
 
-def test_store_three_responses():
+def test_store_three_responses(language_survey):
     """Check if three individual responses are correctly stored."""
-    question = "What is your native language?"
-    language_survey = AnonymousSurvey(question)
     responses = ['English', 'Spanish', 'Polish']
     for response in responses:
         language_survey.store_response(response)
+        
     for response in responses:
         assert response in language_survey.responses
+
+
+
