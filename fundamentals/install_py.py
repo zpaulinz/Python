93e87b72f03fd23ab09c9fcf963c703bb3237c0f
zpaulinz <zpaulinz@gmail.com>
2025-02-03 22:06:13 +0100

    Create install_py.py

---
+++ fundamentals/install_py.py
@@ -0,0 +1,127 @@
+# Install Python:
+
+# Windows, macOS: https://www.python.org/ 
+# Add python.exe to PATH !!!
+
+
+# Linux:
+# default version: $ sudo apt install python3-dev python3-pip python3-venv
+
+# new version: 
+# $ sudo add-apt-repository ppa:deadsnakes/ppa
+# $ sudo apt update
+# $ sudo apt install python3.12
+# $ sudo apt install python3.12-dev python3.12-venv
+
+
+# $ python --version
+
+
+# Python Keywords:
+# False 
+# await 
+# else 
+# import 
+# pass
+# None 
+# break 
+# except 
+# in 
+# raise
+# True 
+# class 
+# finally 
+# is 
+# return
+# and 
+# continue 
+# for 
+# lambda 
+# try
+# as 
+# def 
+# from 
+# nonlocal 
+# while
+# assert 
+# del 
+# global 
+# not 
+# with
+# async 
+# elif 
+# if 
+# or 
+# yield 
+
+# Python built-in functions
+# abs()
+# aiter()
+# all()
+# any()
+# anext()
+# ascii()
+# bin()
+# bool()
+# breakpoint()
+# bytearray()
+# bytes()
+# callable()
+# chr()
+# classmethod()
+# compile()
+# complex()
+# delattr()
+# dict()
+# dir()
+# divmod()
+# enumerate()
+# eval()
+# exec()
+# filter()
+# float()
+# format()
+# frozenset()
+# getattr()
+# globals()
+# hasattr()
+# hash()
+# help()
+# hex()
+# id()
+# input()
+# int()
+# isinstance()
+# issubclass()
+# iter()
+# len()
+# list()
+# locals()
+# map()
+# memoryview()
+# min()
+# next()
+# object()
+# oct()
+# open()
+# ord()
+# pow()
+# print()
+# property()
+# range()
+# repr()
+# reversed()
+# round()
+# set()
+# setattr()
+# slice()
+# sorted()
+# staticmethod()
+# str()
+# sum()
+# super()
+# tuple()
+# type()
+# vars()
+# zip()
+# __import__()
